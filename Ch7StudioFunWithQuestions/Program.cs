e203e94060b87ca9e051b1a7de93489bd04948dd
mmrichmond6 <mmrichmond6@gmail.com>
2022-09-01 12:01:45 -0500

    Started

--- Ch7StudioFunWithQuestions/Program.cs
+++ Ch7StudioFunWithQuestions/Program.cs
@@ -2,7 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        //
+        Console.WriteLine("Welcome to Fun With Questions!");
+        //The Main method within Program will do four things:
+        //* Create several questions
+        //* Present questions to the user (random or user select type?)
+        //* Accept user response
+        //* Respond to answer given (correct or incorrect) and Score
+
     }
 }
